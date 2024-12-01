7e46d3971b684c56dc80dba1fc709cdaee46425c
Ravi Ranjan  Ojha <raviranjanojha999@gmail.com>
2024-12-01 23:59:12 +0530

    init

---
+++ Sources/swift-sdk/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  swift-sdk
+//
+//  Created by Ravi Ranjan  Ojha on 01/12/24.
+//
+
+import Foundation
+
+
+public struct File {
+    public static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
